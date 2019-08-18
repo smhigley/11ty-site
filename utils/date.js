9cfafe320dc805ad1278055293ddbdebff59d979
Sarah Higley <sarah.higley@microsoft.com>
2019-08-17 17:39:26 -0700

    fix date, add current page style

--- utils/date.js
+++ utils/date.js
@@ -1,6 +1,6 @@
 module.exports = function formatDate(value) {
   const date = new Date(value);
-  console.log('incoming date:', value, 'processed js date:', date);
+  const localDate = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
 
-  return date.toLocaleDateString('en-us', {day: 'numeric', month: 'long', year: 'numeric'});
+  return localDate.toLocaleDateString('en-us', {day: 'numeric', month: 'long', year: 'numeric'});
 };
